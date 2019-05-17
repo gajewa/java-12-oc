4c232ba446fd5098140ec5f9c2953c699bbcb66c
Daniel Żukowski <daniel.zukowski@jitsolutions.pl>
2019-05-17 14:58:54 +0200

    Initial commit - spring boot application setup for smart fitting room

---
+++ src/main/java/jit/team/smartFittingRoomLocator/SmartFittingRoomLocatorApplication.java
@@ -0,0 +1,13 @@
+package jit.team.smartFittingRoomLocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFittingRoomLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFittingRoomLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jit/team/smartFittingRoomLocator/SmartFittingRoomLocatorApplicationTests.java
@@ -0,0 +1,16 @@
+package jit.team.smartFittingRoomLocator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartFittingRoomLocatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
